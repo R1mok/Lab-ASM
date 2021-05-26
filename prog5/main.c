1262a97027ef4483bffb8ba62690c6a10a52dddb
Student <b1951306@debian>
2021-05-26 22:31:49 +0300

    and another one

--- prog5/main.c
+++ prog5/main.c
@@ -23,7 +23,7 @@ int convert(char *input, char *output) {
 	int t = clock();
     data = reflect(data, x, y, n);
 	t = clock() - t;
-    if (!stbi_write_jpg(output, x, y, n, data, 100))
+    if (!stbi_write_png(output, x, y, n, data, 100))
         return -2;
     stbi_image_free(data);
     return t;
